80858bb2458ade3dfddf0802359fb6f94a8de5c6
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2025-04-21 13:48:45 -0400

    changed filename for running twitter bot

---
+++ backend/run_bot.py
@@ -0,0 +1,33 @@
+from backend.schedule.get_schedule import get_nba_schedule, get_team_ids
+from backend.db.queries.revenge_games import get_revenge_games
+from backend.bot.twitter_bot import login_to_twitter, post_tweet, format_revenge_tweet
+from backend.scrapers.injury_scrapers import get_nba_injuries
+from selenium import webdriver
+
+def run():
+    # get todays schedule and potential revenge games
+    daily_schedule = get_nba_schedule()  
+    team_id_list = get_team_ids(daily_schedule)  
+    revenge_games = get_revenge_games(team_id_list)
+
+    # injury updates, if player is out dont tweet
+    updated_revenge_games = get_nba_injuries(revenge_games)
+    revenge_games_to_tweet = [game for game in updated_revenge_games if game[2] != "Out"]
+    
+    # print(revenge_games_to_tweet)
+    if len(revenge_games_to_tweet):
+        try:
+            driver = webdriver.Chrome() 
+            login_to_twitter(driver)  
+            
+            for game in revenge_games_to_tweet:
+                tweet = format_revenge_tweet(game)
+                post_tweet(driver, tweet) 
+        finally:
+            driver.quit() # close browser 
+    else: 
+        print("No revenge games today!")
+
+if __name__ == "__main__":
+    run()
+ 
